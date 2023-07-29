88fe292347dbef175fa97d41a4047671eb163467
kevallad02 <kevallad2002@gmail.com>
2023-07-29 20:24:34 +0530

    services

--- services/admin.service.js
+++ services/admin.service.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios")
 const API_URL = 'https://cyberforest.vercel.app/api/'
 
 const adminLoginService = (data) => {
-    return axios.post(API_URL + 'adminLogin', data)
+    return axios.post('api/admin/login', data)
 }
 
 export default {
